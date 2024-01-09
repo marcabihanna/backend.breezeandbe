c8b7b7c8e4d683467341cc57959c6cc010e8de0c
marcabihanna <marcabihanna@gmail.com>
2024-01-09 18:35:55 +0200

    Fix phone prefix

--- app/Http/Traits/GeneralTrait.php
+++ app/Http/Traits/GeneralTrait.php
@@ -34,31 +34,4 @@ trait GeneralTrait
         // return $this->apiResponse(null, false, $message, 200);
         return $this->apiResponse(null, false, $message, 400);
     }
-
-    public function send_email(MailerInterface $mailer, $templateName, $email1, $subj, $order)
-    {
-        try {
-            $email = (new Email())
-            ->from($_ENV['MAIL_FROM_ADDRESS'])
-            ->to($email1)
-            ->replyTo($_ENV['MAIL_FROM_ADDRESS'], $_ENV['MAIL_FROM_NAME'])
-            ->subject($subj)
-            ->html('<h1>Your Order Details</h1>' . $order);
-
-            $transport = (new \Symfony\Component\Mailer\Transport\Smtp\SmtpTransport(
-                $_ENV['MAIL_HOST'],
-                $_ENV['MAIL_PORT'],
-                $_ENV['MAIL_ENCRYPTION']
-            ))
-                ->setUsername($_ENV['MAIL_USERNAME'])
-                ->setPassword($_ENV['MAIL_PASSWORD']);
-
-            $mailer->send($email, $transport);
-
-            return true;
-        } catch (\Exception $e) {
-            // Handle exception as needed
-            return false;
-        }
-    }
 }
